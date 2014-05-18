3968905458eb93c046a93e3c8ef1b3c3ba210dfa
Nobuto MURATA <nobuto@nobuto-murata.org>
2014-05-19 05:04:55 +0900

    Create user.js

---
+++ .mozilla/firefox/user.js
@@ -0,0 +1,24 @@
+//
+
+user_pref("browser.startup.homepage", "about:home");
+
+user_pref("plugins.click_to_play", true);
+user_pref("plugin.default.state", 1);
+user_pref("image.animation_mode", "once");
+user_pref("media.autoplay.enabled", false);
+
+user_pref("passhash.optHashWordSizeDefault", 16);
+
+user_pref("font.default.x-unicode", "sans-serif");
+user_pref("font.name.monospace.x-unicode", "DejaVu Sans Mono");
+user_pref("font.name.sans-serif.x-unicode", "DejaVu Sans");
+user_pref("font.name.serif.x-unicode", "DejaVu Serif");
+
+user_pref("font.default.x-western", "sans-serif");
+user_pref("font.name.monospace.x-western", "DejaVu Sans Mono");
+user_pref("font.name.sans-serif.x-western", "DejaVu Sans");
+user_pref("font.name.serif.x-western", "DejaVu Serif");
+
+user_pref("font.name.monospace.ja", "DejaVu Sans Mono");
+user_pref("font.name.sans-serif.ja", "DejaVu Sans");
+user_pref("font.name.serif.ja", "DejaVu Serif");
