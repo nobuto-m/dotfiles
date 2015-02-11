5fc60a7e516b2f81c699cc1310b3618d60430e92
Nobuto Murata <nobuto@ubuntu.com>
2015-02-11 15:48:56 +0900

    fix LXC container looses static IP issue

--- setup-proxy.sh
+++ setup-proxy.sh
@@ -16,10 +16,16 @@ if ! (sudo lxc-ls --running | grep -q -w squid-deb-proxy); then
         --release trusty --userdata "$USERDATA"
 
     ## set static IP address and autostart
+    cat <<EOF | sudo tee /var/lib/lxc/squid-deb-proxy/rootfs/etc/network/interfaces.d/eth0.cfg
+# The primary network interface
+auto eth0
+iface eth0 inet static
+    address 10.0.7.2
+    netmask 255.255.255.0
+    gateway 10.0.7.1
+    dns-nameservers 10.0.7.1
+EOF
     cat <<EOF | sudo tee -a /var/lib/lxc/squid-deb-proxy/config
-
-lxc.network.ipv4 = 10.0.7.2/24
-lxc.network.ipv4.gateway = 10.0.7.1
 lxc.start.auto = 1
 EOF
 fi
