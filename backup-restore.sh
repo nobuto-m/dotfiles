f9016eb51704f784e91d44b42d6ea52fe1790e27
Nobuto Murata <nobuto@ubuntu.com>
2015-03-01 14:58:09 +0900

    create dirs first

--- backup-restore.sh
+++ backup-restore.sh
@@ -9,8 +9,10 @@ for path in `cat backup.list`; do
     backup="$HOME/backup/home/nobuto/$path"
     target="$HOME/$path"
     if [ -d "$backup" ]; then
+        mkdir -p "$target"
         cp -irv "$backup"/* "$target"
     else
+        mkdir -p "$(dirname "$target")"
         cp -iv "$backup" "$target"
     fi
 done
