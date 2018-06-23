541f7cc262a9218f4283010b87c47a84bec3adbb
Nobuto Murata <nobuto@ubuntu.com>
2018-06-23 22:57:26 +0900

    more keys

--- roles/common/files/xkeysnail.config.py
+++ roles/common/files/xkeysnail.config.py
@@ -1,17 +1,28 @@
 # -*- coding: utf-8 -*-
 
+from xkeysnail.transform import define_keymap
+from xkeysnail.transform import define_modmap
+from xkeysnail.transform import K
+from xkeysnail.transform import Key
 import re
-from xkeysnail.transform import *
 
-# [Global modemap] Change modifier keys as in xmodmap
-define_modmap({Key.CAPSLOCK: Key.LEFT_CTRL})
+# [Global modemap]
+define_modmap({
+    Key.CAPSLOCK: Key.LEFT_CTRL  # xkb-options=['ctrl:nocaps']
+})
 
 # Keybindings for Firefox
 define_keymap(
-    re.compile("^Firefox$"), {
-        K("C-n"): K("DOWN"),
-        K("C-b"): K("LEFT"),
-    }, "Firefox")
+    re.compile("^Firefox$"),
+    {
+        K("C-n"): K("DOWN"),  # override Firefox "New Window"
+        K("C-b"): K("LEFT"),  # override Gmail, Google Docs "Bold"
+        K("C-p"): K("UP"),  # override Google Docs "Print"
+        K("C-f"): K("RIGHT"),  # override Google Docs "Find"
+        K("C-h"): K("BACKSPACE"),  # override Google Docs "Find and replace"
+        K("C-Shift-f"): K("C-f"),  # Firefox "Find in this page"
+    },
+    "Firefox")
 
 # Keybindings for VS Code
 define_keymap(
@@ -20,4 +31,7 @@ define_keymap(
         K("C-n"): K("DOWN"),
         K("C-b"): K("LEFT"),
         K("C-f"): K("RIGHT"),
+        K("C-a"): K("HOME"),
+        K("C-e"): K("END"),
+        K("C-h"): K("BACKSPACE"),
     }, "VS Code")
