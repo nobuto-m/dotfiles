578c36c8d742b09c9ae72c5743c653d927845dea
Nobuto Murata <nobuto@ubuntu.com>
2019-05-28 10:57:20 +0900

    media.autoplay.enabled seems default now
    Also,
    - restore a previous session by default
    - block location access by default

--- roles/user/files/dotfiles/.mozilla/firefox/default/user.js
+++ roles/user/files/dotfiles/.mozilla/firefox/default/user.js
@@ -2,14 +2,14 @@
 
 user_pref("browser.uidensity", 1);
 
+user_pref("browser.sessionstore.warnOnQuit", true);
+user_pref("browser.startup.page", 3);
 user_pref("browser.startup.homepage", "about:home");
 user_pref("browser.newtabpage.activity-stream.showTopSites", false);
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
 
 // HiDPI tweak
 user_pref("layout.css.devPixelsPerPx", "1.8");
-
-user_pref("media.autoplay.enabled", false);
 
 user_pref("layout.spellcheckDefault", 2);
 user_pref("spellchecker.dictionary", "en-US");
@@ -20,6 +20,7 @@ user_pref("browser.contentblocking.category", "strict");
 user_pref("privacy.trackingprotection.enabled", true);
 user_pref("network.cookie.cookieBehavior", 4);
 user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.geo", 2);
 user_pref("accessibility.force_disabled", 1);
 
 user_pref("services.sync.engine.history", false);
