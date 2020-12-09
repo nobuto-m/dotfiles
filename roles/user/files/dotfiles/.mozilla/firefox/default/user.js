b05ff66d50eae5080a79d868642b5e672035ca10
Nobuto Murata <nobuto@ubuntu.com>
2020-12-09 09:46:26 +0900

    enable HTTPS only mode

--- roles/user/files/dotfiles/.mozilla/firefox/default/user.js
+++ roles/user/files/dotfiles/.mozilla/firefox/default/user.js
@@ -31,11 +31,14 @@ user_pref("findbar.highlightAll", true);
 
 user_pref("middlemouse.paste", false);
 
+// Privacy and Security
 user_pref("browser.contentblocking.category", "strict");
 user_pref("permissions.default.geo", 2);  // deny by default
 user_pref("media.autoplay.default", 5);  // block autoplay of audio and video
 user_pref("accessibility.force_disabled", 1);
+user_pref("dom.security.https_only_mode", true);
 
+// Firefox Sync
 user_pref("services.sync.engine.history", false);
 user_pref("services.sync.engine.prefs", false);
 user_pref("services.sync.engine.tabs", false);
