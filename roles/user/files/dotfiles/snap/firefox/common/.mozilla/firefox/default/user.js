202ac00502b76423ee69f6c6432e767c7ccfd461
Nobuto Murata <nobuto@ubuntu.com>
2022-01-14 09:24:57 +0900

    Drop explicit flags and let's rely on the default values now

--- roles/user/files/dotfiles/snap/firefox/common/.mozilla/firefox/default/user.js
+++ roles/user/files/dotfiles/snap/firefox/common/.mozilla/firefox/default/user.js
@@ -1,11 +1,11 @@
 //
 
 // experimental
-user_pref("gfx.webrender.all", true);
-user_pref("gfx.webrender.compositor", true);
+//user_pref("gfx.webrender.all", true);
+//user_pref("gfx.webrender.compositor", true);
 // unavailable by runtime: Wayland support missing
 //user_pref("gfx.webrender.compositor.force-enabled", true);
-user_pref("gfx.x11-egl.force-enabled", true);
+//user_pref("gfx.x11-egl.force-enabled", true);
 
 // legacy
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
