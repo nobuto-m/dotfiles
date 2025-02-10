5b789e77903abe6bfd265011de42e0562e568803
Nobuto Murata <nobuto@ubuntu.com>
2025-02-10 09:43:16 +0900

    to follow the update of wm_class in the snap

--- roles/common/files/xkeysnail.config.py
+++ roles/common/files/xkeysnail.config.py
@@ -18,7 +18,7 @@ define_keymap(None, {
 
 # Keybindings for Web browser
 define_keymap(
-    re.compile("^(firefox|Chromium)$"),
+    re.compile("^(firefox_firefox|Chromium)$"),
     {
         K("C-n"): K("DOWN"),  # override Firefox "New Window"
         K("C-b"): K("LEFT"),  # override Gmail, Google Docs "Bold"
